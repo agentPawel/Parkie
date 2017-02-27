46f9994a8798f8e777a2242dcf45f205a4cf48d4
Pawel Parafianowicz <pawelp@outlook.com>
2017-02-27 15:17:58 -0500

    Migrate and create Participant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227193558) do
+ActiveRecord::Schema.define(version: 20170227201613) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "name"
@@ -26,11 +26,32 @@ ActiveRecord::Schema.define(version: 20170227193558) do
     t.datetime "updated_at",      null: false
   end
 
+  create_table "park_activities", force: :cascade do |t|
+    t.integer  "park_id"
+    t.integer  "activity_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "parks", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "participants", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer  "parkactivity_id"
+    t.integer  "user_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "users", force: :cascade do |t|
