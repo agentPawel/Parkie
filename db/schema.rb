8e02c42e6aac31eb5906a605beaae0ab09d80111
Pawel Parafianowicz <pawelp@outlook.com>
2017-02-27 14:07:43 -0500

    Generate Park model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227184853) do
+ActiveRecord::Schema.define(version: 20170227190717) do
+
+  create_table "parks", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
