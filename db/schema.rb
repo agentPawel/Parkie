0547e079714368638cd6bc83ef1567c33d8e3f50
Pawel Parafianowicz <pawelp@outlook.com>
2017-02-27 15:22:45 -0500

    Remove Participant model, create EventAttendance model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227201613) do
+ActiveRecord::Schema.define(version: 20170227194409) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "name"
@@ -36,13 +36,6 @@ ActiveRecord::Schema.define(version: 20170227201613) do
   create_table "parks", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "participants", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
