6d95905dd51f7b764129057f89461e2f5dbe85fa
Pawel Parafianowicz <pawelp@outlook.com>
2017-03-01 14:01:59 -0500

    Update user validation to require Username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :email, presence: true
   validates :username, presence: true
+  validates :username, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
