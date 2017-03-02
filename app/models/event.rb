1ef53c09571fbf159781142458a0d0132720c96c
Pawel Parafianowicz <pawelp@outlook.com>
2017-03-01 19:55:05 -0500

    Fix relationships in event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :owner, class_name: "User"
+  belongs_to :owner, class_name: "User", foreign_key: "user_id"
   belongs_to :park_activities
   has_many :participants
   has_many :attendees, class_name: "User", through: :participants, source: :user
