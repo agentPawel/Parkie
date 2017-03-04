43da3e28b4cfaae4a0238d07b61092411b504c56
Valerie Boem <vboem@gmail.com>
2017-03-04 17:03:02 -0500

    Added to subscriptions controller create method

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -4,7 +4,16 @@ class SubscriptionsController < ApplicationController
   end
 
   def create
-    @subscription = Subscription.new
-    @subscription.save
+    @subscription = Subscription.new(subscription_params)
+    if @subscription.save
+      redirect_to "root_url"
+    else
+      render "new"
   end
+end
+
+private
+
+def subscription_params
+  params.require(:subscription).permit(:park_activity_id, :user_id)
 end
