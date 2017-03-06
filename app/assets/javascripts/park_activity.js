f99ca3226b7082c8166855088a6544fc1437c94d
Pawel Parafianowicz <pawelp@outlook.com>
2017-03-06 16:38:17 -0500

    Edit subscribe button code and activities show page

--- app/assets/javascripts/park_activity.js
+++ app/assets/javascripts/park_activity.js
@@ -1,14 +1,26 @@
 $(function(){
 
-  $('.subscribe_button').on('click',(function(event){
+  $('button').on('click',(function(event){
+
+    var thisButton = this;
 
     $.ajax({
       url: subscriptions_path,
       method:'POST'
       data: {
-            park_activity_id: ParkActivity.find(params[:id]).id,
-            user_id: current_user.id },
-      dataType: 'json' //json, text, html
+        park_activity_id: ParkActivity.find(params[:id]).id,
+        user_id: current_user.id
+      },
+      dataType: 'JSON'
+    })
+
+    .success(function(){
+      console.log("done");
+      $(thisButton).html("Unsubscribe");
+      $(thisButton).hide();
+    });
+  });
+});
 
 
 //     }).done(function(data){
