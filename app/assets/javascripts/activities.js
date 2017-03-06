a2f5a9d02745017797256c6eef406bef0d7a82bb
Pawel Parafianowicz <pawelp@outlook.com>
2017-03-06 16:39:35 -0500

    Create activities js file

---
+++ app/assets/javascripts/activities.js
@@ -0,0 +1,23 @@
+$(function(){
+
+  $('button').on('click',(function(event){
+
+    var thisButton = this;
+
+    $.ajax({
+      url: subscriptions_path,
+      method:'POST'
+      data: {
+        park_activity_id: ParkActivity.find(params[:id]).id,
+        user_id: current_user.id
+      },
+      dataType: 'JSON'
+    })
+
+    .success(function(){
+      console.log("done");
+      $(thisButton).html("Unsubscribe");
+      $(thisButton).hide();
+    });
+  });
+});
