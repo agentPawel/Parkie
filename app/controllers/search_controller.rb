7741f95641d3f405bdda657589db3c4b55d7e201
frankburke333 <frank.burke@gmail.com>
2017-03-06 21:01:58 -0500

    add functional home page with parks near current location listed and map with seeded pins showing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,6 +12,20 @@ class SearchController < ApplicationController
     Geocode.search("@address")
   end
 
+  def parks_near_by
+    gps = params[:latitude]+','+ params[:longitude]
+    Park.near(gps, 1.9).size
+    respond_to do |format|
+      format.json do
+        render :json => Park.near(gps, 1.9)
+      end
+    end
+    # @parks.each do |park|
+    #   park.nearbys(1.9).each do |near_park|
+    #   link_to near_park.name near_park.distance.round(2) kms)
+    # end
+  end
+
 
 
 end
