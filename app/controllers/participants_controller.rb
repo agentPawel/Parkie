767e05e46a32197351177d2fc91fbc5ec73a69a2
g-mehra <gmehra@alumni.uwo.ca>
2017-03-07 16:24:37 -0500

    Added notification functionality for participants and owners upon event join cancellation:

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -22,9 +22,19 @@ class ParticipantsController < ApplicationController
     user = current_user.id
     event = params[:event]
     @participant = Participant.find_by(user_id: user, event_id: event)
-    @participant.destroy
-    Event.find(event).count -= 1
-    redirect_to user_path(current_user.id)
+    if @participant.destroy
+      Event.find(event).count -= 1
+      if current_user.cell != nil
+        participant_cancel_message
+      end
+      if Event.find(params[:event]).owner.cell != nil
+        owner_participant_cancel_message
+      end
+      redirect_to user_path(current_user.id)
+    else
+      redirect_to user_path(current_user.id)
+    end
+
   end
 
   def participant_event_join_message
@@ -33,6 +43,15 @@ class ParticipantsController < ApplicationController
     park = Event.find(params[:event]).park.name
     time = Event.find(params[:event]).date_time
     body = "PARKIE: You've joined #{owner}'s #{activity} event at #{park} for #{time.strftime("%I:%M%p")}!"
+    Message.send_message(current_user.cell, body)
+  end
+
+  def participant_cancel_message
+    owner = Event.find(params[:event]).owner.username
+    activity = Event.find(params[:event]).activity.name
+    park = Event.find(params[:event]).park.name
+    time = Event.find(params[:event]).date_time
+    body = "PARKIE: You've canceled your participation for #{owner}'s #{activity} event at #{park} for #{time.strftime("%I:%M%p")}!"
     Message.send_message(current_user.cell, body)
   end
 
@@ -46,6 +65,18 @@ class ParticipantsController < ApplicationController
     body = "PARKIE: Hi #{owner}! #{participant} has just joined your #{activity} event at #{park} for #{time.strftime("%I:%M%p")}!"
     Message.send_message(owner_cell, body)
   end
+
+  def owner_participant_cancel_message
+    owner = Event.find(params[:event]).owner.username
+    owner_cell = Event.find(params[:event]).owner.cell
+    park = Event.find(params[:event]).park.name
+    time = Event.find(params[:event]).date_time
+    activity = Event.find(params[:event]).activity.name
+    participant = current_user.username
+    body = "PARKIE: Hi #{owner}! #{participant} has just cancelled their participation for your #{activity} event at #{park} for #{time.strftime("%I:%M%p")}!"
+    Message.send_message(owner_cell, body)
+  end
+
 
 
 
