8ac9fc6258b45cf8ea68c5e047210ef9473af97b
frankburke333 <frank.burke@gmail.com>
2017-03-07 17:05:48 -0500

    add re-factored maps logic

--- app/assets/javascripts/main_map.js
+++ app/assets/javascripts/main_map.js
@@ -6,8 +6,6 @@ jQuery(function($) {
 });
 
 function initialize() {
-    var map;
-    var bounds = new google.maps.LatLngBounds();
     var mapOptions = {
         mapTypeId: 'roadmap',
         scrollwheel: false,
@@ -18,62 +16,5 @@ function initialize() {
     map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
     map.setTilt(45);
 
-    // Multiple Markers
-    var markers = [
-      ['Art Eggleton Park', 43.6595529, -79.418166],
-      ['Healy Willan', 43.65921729999999, -79.4131654],
-      ['Christie Pits', 43.66480840000001, -79.4207052],
-      ['Fred Hamilton Park', 43.6527404, -79.4204378],
-      ['Dufferin Grove', 43.6563342, -79.432311],
-      ['Trinity Bellwoods Park', 43.6454767, -79.4138873],
-      ['Grange Park', 43.6524408, -79.3915032],
-      ['Alexandra Park', 43.6511083, -79.4050451],
-      ['Clarence Square Park', 43.64444599999999, -79.3936626],
-      ['Bellevue Square', 43.6537303, -79.4019251],
-      ['Margaret Fairley Park', 43.6603421, -79.40547649999999]
-    ];
-
-    // Info Window Content
-    var infoWindowContent = [
-        ['<div class="info_content">' +
-        '<h3>London Eye</h3>' +
-        '<p>The London Eye is a giant Ferris wheel situated on the banks of the River Thames. The entire structure is 135 metres (443 ft) tall and the wheel has a diameter of 120 metres (394 ft).</p>' +        '</div>'],
-        ['<div class="info_content">' +
-        '<h3>Palace of Westminster</h3>' +
-        '<p>The Palace of Westminster is the meeting place of the House of Commons and the House of Lords, the two houses of the Parliament of the United Kingdom. Commonly known as the Houses of Parliament after its tenants.</p>' +
-        '</div>']
-    ];
-
-    // Display multiple markers on a map
-    var infoWindow = new google.maps.InfoWindow(), marker, i;
-
-    // Loop through our array of markers & place each one on the map
-    for( i = 0; i < markers.length; i++ ) {
-        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
-        bounds.extend(position);
-        console.log(position)
-        marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            title: markers[i][0]
-        });
-
-        // Allow each marker to have an info window
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-                infoWindow.setContent(infoWindowContent[i][0]);
-                infoWindow.open(map, marker);
-            }
-        })(marker, i));
-
-        // Automatically center the map fitting all markers on the screen
-        map.fitBounds(bounds);
-    }
-
-    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
-    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-        this.setZoom(14);
-        google.maps.event.removeListener(boundsListener);
-    });
 
 }
