83184b2e8aabec04691f78f6f67752dbc4cb5f5d
g-mehra <gmehra@alumni.uwo.ca>
2017-03-07 18:17:02 -0500

    Add search bar to parks

--- app/models/park.rb
+++ app/models/park.rb
@@ -6,4 +6,18 @@ class Park < ApplicationRecord
   # accepts_nested_attributes_for :activities
   geocoded_by :address       # can also be an IP address
   after_validation :geocode, :if => :address_changed?
+
+
+
+
+  def self.search(search)
+    where("name LIKE ?", "%#{search}%")
+  end
+
+
+
+
+
+
+
 end
