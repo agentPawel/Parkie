6d6f8d462dbef81193e8edb23402b11cc1b472d3
g-mehra <gmehra@alumni.uwo.ca>
2017-03-09 19:39:38 -0500

    Add image attribute migration to activities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309221145) do
+ActiveRecord::Schema.define(version: 20170310000823) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "name"
